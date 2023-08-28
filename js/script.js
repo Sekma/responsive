10dc36d693525937e610360d90699349cf42a905
Mohamed Hedi Sekma <sekma.mohamed.hedi@gmail.com>
2023-08-29 00:29:52 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+   
+    const burgerToggle = document.querySelector('.burger-toggle');
+
+    burgerToggle.classList.toggle('active');
+      const nav = document.querySelector('nav');
+      nav.classList.toggle('active');
+  }
+
