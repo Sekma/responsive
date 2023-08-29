923b9488e58baf5d7b016e61d205fb6bfb0a7e94
Mohamed Hedi Sekma <sekma.mohamed.hedi@gmail.com>
2023-08-29 23:33:14 +0100

    correction bouton menu

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 function toggleMenu() {
    
     const burgerToggle = document.querySelector('.burger-toggle');
+    const nav = document.querySelector('nav');
 
     burgerToggle.classList.toggle('active');
-      const nav = document.querySelector('nav');
-      nav.classList.toggle('active');
+    nav.classList.toggle('active');
   }
+
 
